3d23587f59f21aa5498368275ff84742d8e363bd
David Cogbill <d_cogbill@yahoo.com>
2023-06-05 17:35:07 -0400

    [Added insertHero() and getHeroById() methods]

--- RevProjectZero/src/main/java/com/revature/daos/HeroDAO.java
+++ RevProjectZero/src/main/java/com/revature/daos/HeroDAO.java
@@ -3,10 +3,7 @@ package com.revature.daos;
 import com.revature.models.Hero;
 import com.revature.util.ConnectionUtil;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class HeroDAO implements HeroDAOInterface{
@@ -44,11 +41,50 @@ public class HeroDAO implements HeroDAOInterface{
 
     @Override
     public Hero getHeroById(int id) {
+        try(Connection conn = ConnectionUtil.getConnection()){
+
+            String sql = "SELECT * FROM heroes WHERE hero_id = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1,id);
+
+            ResultSet rs = ps.executeQuery();
+
+            if ( rs.next() ){
+                Hero hero = new Hero(
+                        rs.getInt("hero_id"),
+                        rs.getString("hero_name"),
+                        rs.getString("hero_weapon")
+                );
+                System.out.println(hero);
+                return hero;
+            }
+
+        }
+        catch(SQLException e){
+            System.out.println("Error getting Hero!");
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Hero insertHero(Hero hero) {
+        try(Connection conn = ConnectionUtil.getConnection()){
+
+            String sql = "INSERT INTO heroes (hero_name, hero_weapon) VALUES (?, ?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, hero.getHero_name());
+            ps.setString(2, hero.getHero_weapon());
+
+            ps.executeUpdate();
+
+            return hero;
+            
+        } catch(SQLException e){
+            System.out.println("Insert Hero failed!");
+            e.printStackTrace();
+        }
         return null;
     }
 }
